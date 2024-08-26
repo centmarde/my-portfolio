b3e080513d120b1e214c3c5fefdaf170a3604e20
whitelotus19 <159101935+centmarde@users.noreply.github.com>
2024-08-26 22:35:31 +0800

    fix supabas logo

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -98,7 +98,7 @@
             <div class="row">
               <div class="col text-center mt-3"><img class="mx-1" src="../assets/HTML.png" style="width: 2rem;"
                   alt="HTML">
-                <img class="mx-1" src="../assets/supabase.png" style="width: 2rem;" alt="supabase">
+                <img class="mx-1" src="../assets/Supabase.png" style="width: 2rem;" alt="supabase">
                 <img class="mx-1" src="../assets/nodejs.png" style="width: 2rem;" alt="Nodjs">
                 <img class="mx-1" src="../assets/Bootstrap_logo.svg.png" style="width: 2rem;" alt="bootstrap">
                 <h3 class="mt-2">Learn-It Mobile App.</h3>
@@ -134,7 +134,7 @@
 
             <div class="row">
               <div class="col d-flex mt-4"><img class="mx-1" src="../assets/HTML.png" style="width: 2.5rem;" alt="HTML">
-                <img class="mx-1" src="../assets/supabase.png" style="width: 2.5rem;" alt="supabase">
+                <img class="mx-1" src="../assets/Supabase.png" style="width: 2.5rem;" alt="supabase">
                 <img class="mx-1" src="../assets/nodejs.png" style="width: 2.5rem;" alt="Nodjs">
                 <img class="mx-1" src="../assets/Bootstrap_logo.svg.png" style="width: 2.5rem;" alt="bootstrap">
               </div>
@@ -225,7 +225,7 @@
             <div class="row">
               <div class="col text-center mt-3"><img class="mx-1" src="../assets/HTML.png" style="width: 2rem;"
                   alt="HTML">
-                <img class="mx-1" src="../assets/supabase.png" style="width: 2rem;" alt="supabase">
+                <img class="mx-1" src="../assets/Supabase.png" style="width: 2rem;" alt="supabase">
                 <img class="mx-1" src="../assets/nodejs.png" style="width: 2rem;" alt="Nodjs">
                 <img class="mx-1" src="../assets/Bootstrap_logo.svg.png" style="width: 2rem;" alt="bootstrap">
                 <h3 class="mt-2">Smart Notes</h3>
@@ -268,7 +268,7 @@
             <h5 class="text-center">
               <div class="col text-center mt-3"><img class="mx-1" src="../assets/HTML.png" style="width: 2rem;"
                   alt="HTML">
-                <img class="mx-1" src="../assets/supabase.png" style="width: 2rem;" alt="supabase">
+                <img class="mx-1" src="../assets/Supabase.png" style="width: 2rem;" alt="supabase">
                 <img class="mx-1" src="../assets/nodejs.png" style="width: 2rem;" alt="Nodjs">
                 <img class="mx-1" src="../assets/Bootstrap_logo.svg.png" style="width: 2rem;" alt="bootstrap">
                
