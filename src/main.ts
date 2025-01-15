6b63386ef07f531c1a338281e011ef395b63a936
centmarde <centmarde.campado@gmail.com>
2025-01-16 01:56:50 +0800

    fix: 🔨 main.ts error

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'vuetify/styles'
 
 // @ts-ignore
-
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
